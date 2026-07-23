7995b7858f0bcd0554dcceb360137bf40425fd44
straumat <stephane.traumat@gmail.com>
2026-07-23 15:26:51 +0200

    Fix a checkstyle issue

--- applications/kiso-cli/src/main/java/com/oakinvest/kiso/cli/Application.java
+++ applications/kiso-cli/src/main/java/com/oakinvest/kiso/cli/Application.java
@@ -4,6 +4,9 @@ import com.oakinvest.kiso.cli.command.BuildCommand;
 import com.oakinvest.kiso.cli.command.CheckCommand;
 import picocli.CommandLine;
 
+/**
+ * Application.
+ */
 @CommandLine.Command(
         name = "kiso-cli",
         mixinStandardHelpOptions = true,
@@ -21,15 +24,12 @@ public class Application implements Runnable {
      * @param args arguments
      */
     public static void main(final String[] args) {
-        int exitCode = new CommandLine(new Application()).execute(args);
+        final int exitCode = new CommandLine(new Application()).execute(args);
         System.exit(exitCode);
     }
 
-    /**
-     * Run.
-     */
     @Override
-    public void run() {
+    public final void run() {
         new CommandLine(this).usage(System.out);
     }
 
